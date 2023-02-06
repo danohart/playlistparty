a2f0ec0f9f4f2a50368af6833d1e96ba6ac8e288
Daniel Hart <me@danielhart.co>
2023-02-06 15:56:27 -0600

    Clean up css, override color on variant "info"

--- pages/welcome.js
+++ pages/welcome.js
@@ -1,5 +1,19 @@
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
 
 export default function Welcome() {
-  return "howdy";
+  return (
+    <Row>
+      <Col className='fade-in'>
+        <h1>Welcome</h1>
+        <Row className='button-group'>
+          <Col>
+            <Button>Get Started</Button>
+          </Col>
+          <Col>
+            <Button variant='outline-info'>Learn More</Button>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
 }
