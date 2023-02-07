39543b6148a5c56412b7d2dc256e5f9b1cb7dbc4
Daniel Hart <me@danielhart.co>
2023-02-07 12:50:24 -0600

    Add playlistId to localStorage to use until it's reset

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import Head from "next/head";
+import React, { useState, useEffect } from "react";
 import { Row, Col, Button, FormControl } from "react-bootstrap";
 import AllPlaylists from "@/compontents/AllPlaylists";
 import Meta from "@/compontents/Meta";
@@ -8,6 +7,11 @@ import { siteTitle } from "@/lib/constants";
 export default function Home() {
   const [playlistId, setPlaylistId] = useState("Select Playlist");
   const [songsToAdd, setSongsToAdd] = useState("");
+
+  useEffect(() => {
+    if (localStorage.getItem("playlistId"))
+      setPlaylistId(localStorage.getItem("playlistId"));
+  }, []);
 
   function addToPlaylist(songs) {
     const songId = songs.split("/", 10)[4].split("?", 1);
@@ -33,6 +37,7 @@ export default function Home() {
   function playlistSelect(e) {
     const selection = e.target.value;
     setPlaylistId(selection);
+    localStorage.setItem("playlistId", selection);
   }
 
   return (
@@ -42,7 +47,10 @@ export default function Home() {
       <Row>
         <Col xs={12} sm={12} md={6} lg={6}>
           <h2>Pick a playlist</h2>
-          <AllPlaylists playlistSelect={playlistSelect} />
+          <AllPlaylists
+            playlistSelect={playlistSelect}
+            playlistId={playlistId}
+          />
         </Col>
         <Col xs={12} sm={12} md={6} lg={6}>
           <Row>
